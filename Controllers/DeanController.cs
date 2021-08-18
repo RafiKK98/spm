31e40ff0f7bf55c7b7a25ec5e558fd85f10ea3c1
hasib <86922590+Hasib1621@users.noreply.github.com>
2021-08-18 16:00:16 +0600

    Dean

--- Controllers/DeanController.cs
+++ Controllers/DeanController.cs
@@ -24,6 +24,7 @@ namespace SpmsApp.Controllers
         [HttpGet("/dean/spcp/")]
         public IActionResult StudentPLOComparisonByProgram()
         {
+            //
             return View();
         }
 
