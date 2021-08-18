31dac64a72b3b57ade227205b415f41531333057
farzanatabassum <82896277+farzanatabassum@users.noreply.github.com>
2021-08-18 22:41:00 +0600

    Firstcommit

---
+++ Controllers/UGCorIEBController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpmsApp.Controllers
+{
+    public class UGCorIEBController : Controller
+    {
+     
+
+        [HttpGet("/ugc/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/ugc/pafap")]
+        public IActionResult PLOAchievementForAProgram()
+        {
+            return View();
+        }
+        [HttpGet("/ugc/pwcp")]
+        public IActionResult PLOwiseCoursePerformance()
+        {
+            return View();
+        }
+        [HttpGet("/ugc/sogaap")]
+        public IActionResult StudentsOrGraduatesAchievingAllPLOs()
+        {
+            return View();
+        }
+
+
+
+    }
+}
