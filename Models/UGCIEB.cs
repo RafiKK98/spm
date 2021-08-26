2371eeaed3032549943e0a52ab6f6d9cc4d08599
farzanatabassum <82896277+farzanatabassum@users.noreply.github.com>
2021-08-26 21:19:57 +0600

    hello

--- Models/UGCIEB.cs
+++ Models/UGCIEB.cs
@@ -5,8 +5,8 @@ namespace SpmsApp.Models
     [Serializable]
     public class UGCIEB : User
     {
-        public int UGCIEB { get; set; }
-        public University University{get; set;}
+        public int UGCIEBID { get; set; }
+        public University University{ get; set;}
 
     }
 }
