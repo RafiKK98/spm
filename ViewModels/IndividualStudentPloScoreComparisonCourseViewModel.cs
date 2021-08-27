90626335622285212c0286158866aa00c74e5e95
Mahfuzur Rahman <84506517+MahfuzurRahmanMoon@users.noreply.github.com>
2021-08-27 17:07:11 +0600

    implemented Individual student PLO score comparison vs Course Average for a selected course

---
+++ ViewModels/IndividualStudentPloScoreComparisonCourseViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using SpmsApp.Models;
+
+namespace SpmsApp.ViewModels
+{
+    public class IndividualStudentPloScoreComparisonCourseViewModel
+    {
+        public TopbarViewModel TopbarViewModel { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
